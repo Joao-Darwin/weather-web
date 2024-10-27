f6eebe651d51daa031ab1acb34feb7787c882b69
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-27 11:25:04 -0300

    feat: Ajustar tamanho do input de pesquisa de cidade

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
+import { Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const Home = (): React.JSX.Element => {
   return (
-    <div>Home</div>
+    <Stack direction="column" alignItems={"center"} spacing={3}>
+      <Typography variant='h3' textAlign={'left'} fontWeight={600}>Previsão do Tempo</Typography>
+      <TextField type='search' placeholder='Insira aqui o nome da cidade' style={{
+        width: '100%',
+        backgroundColor: 'white',
+        borderRadius: 5
+      }} />
+    </Stack>
   )
 }
 
