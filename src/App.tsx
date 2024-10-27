73c86390cc657ebc74a4d8207bdf71ef7e9b6357
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-27 13:44:44 -0300

    feat: Estilizar input de busca de cidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 import About from './pages/About'
 import NavBar from './components/NavBar/NavBar'
-import { Container } from '@mui/material'
+import { Container, Divider } from '@mui/material'
 
 function App() {
   return (
     <BrowserRouter>
       <Container maxWidth="md">
         <NavBar />
+        <Divider color='white' variant='middle' style={{marginBottom: 20}} />
         <Routes>
           <Route path='/' Component={Home} />
           <Route path='/about' Component={About} />
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,33 @@
+import { Box, InputAdornment, Stack, TextField, Typography } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import React from 'react'
+
+import "./Home.css";
+
+const Home = (): React.JSX.Element => {
+  return (
+    <Stack direction="column" alignItems={"center"} spacing={3}>
+      <Box display={"flex"} flexDirection={"column"} gap={3}>
+        <Typography variant='h3' textAlign={'left'} fontWeight={600}>Previsão do Tempo</Typography>
+        <TextField
+          type='search'
+          placeholder='Insira aqui o nome da cidade'
+          className='textField'
+          slotProps={{
+            input: {
+              endAdornment: <InputAdornment
+                position="end"
+                onClick={() => console.log("send request")}
+                className='inputAdornment'
+              >
+                <SearchIcon />
+              </InputAdornment>
+            }
+          }}
+        />
+      </Box>
+    </Stack>
+  )
+}
+
+export default Home
