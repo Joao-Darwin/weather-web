7946ef634d79d0e020ae39eff6a5b1cce652f294
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-27 11:24:48 -0300

    feat: Envolver toda a página em um container

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import NavBar from './components/NavBar/NavBar'
+import { Container } from '@mui/material'
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' Component={Home} />
-        <Route path='/about' Component={About} />
-      </Routes>
+      <Container maxWidth="md">
+        <NavBar />
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/about' Component={About} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   )
 }
