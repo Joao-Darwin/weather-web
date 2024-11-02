31502b7545d31e6bc884d9ed8332381bc02ed9aa
João Darwin <joaodarwin.ip22@gmail.com>
2024-11-02 12:29:24 -0300

    feat: Criar na home, componente com informações completas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { Box, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { Box, IconButton, InputAdornment, Paper, Stack, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 
 import CapitalClimates from '../../components/CapitalClimates/CapitalClimates';
 import CompleteWeather from '../../interfaces/CompleteWeather';
 import RequestApi from '../../services/RequestApi';
 import "./Home.css";
+import { Close } from '@mui/icons-material';
 
 const Home = (): React.JSX.Element => {
   const [weatherCity, setWeatherCity] = useState<CompleteWeather | null>(null);
@@ -23,6 +24,33 @@ const Home = (): React.JSX.Element => {
   return (
     <>
       <Stack direction="column" alignItems={"center"} spacing={6}>
+        {weatherCity &&
+          <Paper sx={{
+            width: {
+              xs: "100%",
+              sm: "70%",
+              md: "60%",
+              lg: "60%",
+              xl: "70%"
+            },
+            height: "400px",
+            padding: {
+              xs: "10px",
+              xl: "2%"
+            },
+            paddingLeft: {
+              sm: "8%",
+              xl: "8%"
+            }
+          }}>
+            <Stack direction={'row'} alignItems={'center'} justifyContent={"space-between"}>
+              <Typography fontWeight={600}>{weatherCity.location.name}, {weatherCity.location.region} - {weatherCity.location.country}</Typography>
+              <IconButton onClick={() => setWeatherCity(null)}>
+                <Close color='secondary' />
+              </IconButton>
+            </Stack>
+          </Paper>
+        }
         <Box display={"flex"} flexDirection={"column"} gap={6}>
           <Typography variant='h3' textAlign={'left'} fontWeight={600}>Weather Web</Typography>
           <TextField
