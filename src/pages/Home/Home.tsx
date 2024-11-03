b04ee2784d1c7a42070ab26ca10e2264d47be71d
João Darwin <joaodarwin.ip22@gmail.com>
2024-11-02 23:04:33 -0300

    feat: Exibir skeleton ao procurar por cidade

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { Alert, Box, Collapse, Divider, IconButton, InputAdornment, Paper, Stack, TextField, Typography } from '@mui/material';
+import { Alert, Box, Collapse, Divider, IconButton, InputAdornment, Paper, Skeleton, Stack, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 import { ArrowDownward, ArrowUpward, Close } from '@mui/icons-material';
@@ -15,18 +15,21 @@ const Home = (): React.JSX.Element => {
   const [weatherCity, setWeatherCity] = useState<CompleteWeather | null>(null);
   const [cityName, setCityName] = useState<string>('');
   const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [isLoadingInfo, setIsLoadingInfo] = useState<boolean>(false);
 
   const handleCityName = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setCityName(event.target.value);
   }
 
   const handleRequest = async () => {
+    setIsLoadingInfo(true);
     try {
       const weather = await RequestApi.getCurrentWeatherWithForecats(cityName);
       setWeatherCity(weather)
     } catch (error) {
       setShowAlert(true)
     }
+    setIsLoadingInfo(false);
   }
 
   const clearCityNameInput = () => {
@@ -47,8 +50,8 @@ const Home = (): React.JSX.Element => {
     <>
       <Stack direction="column" alignItems={"center"} spacing={6}>
         <Typography variant='h3' textAlign={'left'} fontWeight={600}>Weather Web</Typography>
-        {weatherCity &&
-          <Paper sx={{
+        {isLoadingInfo ? (
+          <Skeleton variant='rounded' sx={{
             width: {
               xs: "100%",
               sm: "70%",
@@ -56,89 +59,101 @@ const Home = (): React.JSX.Element => {
               lg: "60%",
               xl: "70%"
             },
-            height: "390px",
-            padding: {
-              xs: "10px",
-              xl: "2%"
-            },
-            paddingLeft: {
-              xs: "8%",
-              sm: "8%",
-              xl: "8%"
-            },
-            paddingRight: {
-              xs: "8%",
-              sm: "8%",
-              xl: "8%"
-            }
-          }}>
-            <Stack spacing={3} textAlign={"start"}>
-              <Stack direction={'row'} alignItems={'center'} justifyContent={"space-between"}>
-                <Typography fontWeight={600}>{weatherCity.location.name}, {weatherCity.location.region} - {weatherCity.location.country}</Typography>
-                <IconButton onClick={() => {
-                  setWeatherCity(null);
-                  clearCityNameInput();
-                }}>
-                  <Close color='secondary' />
-                </IconButton>
-              </Stack>
-              <Typography variant='h3' fontWeight={600}>
-                {weatherCity.current.temperature}ºC {weatherCity.current.condition}
-              </Typography>
-              <Stack direction={"row"} spacing={5}>
-                <Stack direction={"row"} spacing={2}>
-                  <Box flexDirection={"row"} display={"flex"}>
-                    <ArrowDownward />
-                    <Typography fontWeight={600}>{weatherCity.current.mintemp}º</Typography>
-                  </Box>
-                  <Box flexDirection={"row"} display={"flex"}>
-                    <ArrowUpward />
-                    <Typography fontWeight={600}>{weatherCity.current.maxtemp}º</Typography>
-                  </Box>
+            height: "390px"
+          }} />
+        ) : (
+          weatherCity &&
+            <Paper sx={{
+              width: {
+                xs: "100%",
+                sm: "70%",
+                md: "60%",
+                lg: "60%",
+                xl: "70%"
+              },
+              height: "390px",
+              padding: {
+                xs: "10px",
+                xl: "2%"
+              },
+              paddingLeft: {
+                xs: "8%",
+                sm: "8%",
+                xl: "8%"
+              },
+              paddingRight: {
+                xs: "8%",
+                sm: "8%",
+                xl: "8%"
+              }
+            }}>
+              <Stack spacing={3.5} textAlign={"start"}>
+                <Stack direction={'row'} alignItems={'center'} justifyContent={"space-between"}>
+                  <Typography fontWeight={600}>{weatherCity.location.name}, {weatherCity.location.region} - {weatherCity.location.country}</Typography>
+                  <IconButton onClick={() => {
+                    setWeatherCity(null);
+                    clearCityNameInput();
+                  }}>
+                    <Close color='secondary' />
+                  </IconButton>
                 </Stack>
-                <Stack direction={"row"} spacing={1}>
-                  <Typography>
-                    Sensation
-                  </Typography>
-                  <Typography fontWeight={600}>
-                    {weatherCity.current.feelslike}º
-                  </Typography>
+                <Typography variant='h3' fontWeight={600}>
+                  {weatherCity.current.temperature}ºC {weatherCity.current.condition}
+                </Typography>
+                <Stack direction={"row"} spacing={5}>
+                  <Stack direction={"row"} spacing={2}>
+                    <Box flexDirection={"row"} display={"flex"}>
+                      <ArrowDownward />
+                      <Typography fontWeight={600}>{weatherCity.current.mintemp}º</Typography>
+                    </Box>
+                    <Box flexDirection={"row"} display={"flex"}>
+                      <ArrowUpward />
+                      <Typography fontWeight={600}>{weatherCity.current.maxtemp}º</Typography>
+                    </Box>
+                  </Stack>
+                  <Stack direction={"row"} spacing={1}>
+                    <Typography>
+                      Sensation
+                    </Typography>
+                    <Typography fontWeight={600}>
+                      {weatherCity.current.feelslike}º
+                    </Typography>
+                  </Stack>
+                </Stack>
+                <Stack direction={"row"} spacing={10}>
+                  <Stack direction={"row"} spacing={1}>
+                    <Typography>
+                      Wind
+                    </Typography>
+                    <Typography fontWeight={600}>
+                      {weatherCity.current.wind_kph}km/h
+                    </Typography>
+                  </Stack>
+                  <Stack direction={"row"} spacing={1}>
+                    <Typography>
+                      Humidity
+                    </Typography>
+                    <Typography fontWeight={600}>
+                      {weatherCity.current.humidity}º
+                    </Typography>
+                  </Stack>
+                </Stack>
+                <Divider color='primary' />
+                <Stack direction={"row"} justifyContent={'space-between'}>
+                  {weatherCity.forecast.map((forecast, index) => {
+                    return (
+                      <SimpleForecast
+                        key={index}
+                        date={forecast.date}
+                        mintemp={forecast.mintemp}
+                        maxtemp={forecast.maxtemp}
+                      />
+                    )
+                  })}
                 </Stack>
               </Stack>
-              <Stack direction={"row"} spacing={10}>
-                <Stack direction={"row"} spacing={1}>
-                  <Typography>
-                    Wind
-                  </Typography>
-                  <Typography fontWeight={600}>
-                    {weatherCity.current.wind_kph}km/h
-                  </Typography>
-                </Stack>
-                <Stack direction={"row"} spacing={1}>
-                  <Typography>
-                    Humidity
-                  </Typography>
-                  <Typography fontWeight={600}>
-                    {weatherCity.current.humidity}º
-                  </Typography>
-                </Stack>
-              </Stack>
-              <Divider color='primary' />
-              <Stack direction={"row"} justifyContent={'space-between'}>
-                {weatherCity.forecast.map((forecast, index) => {
-                  return (
-                    <SimpleForecast
-                      key={index}
-                      date={forecast.date}
-                      mintemp={forecast.mintemp}
-                      maxtemp={forecast.maxtemp}
-                    />
-                  )
-                })}
-              </Stack>
-            </Stack>
-          </Paper>
-        }
+            </Paper>
+        )}
         <Box display={"flex"} flexDirection={"column"} gap={6}>
           <TextField
             type='text'
