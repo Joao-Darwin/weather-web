856e5fff816142c71ddc9f1e686d315971d3fbda
João Darwin <joaodarwin.ip22@gmail.com>
2024-11-08 17:09:30 -0300

    feat: Ordenar nome da capital para minha coluna na tabela

--- src/components/CapitalClimates/CapitalClimates.tsx
+++ src/components/CapitalClimates/CapitalClimates.tsx
@@ -1,8 +1,8 @@
 import AirIcon from '@mui/icons-material/Air';
 import CloudIcon from '@mui/icons-material/Cloud';
-import ThermostatIcon from '@mui/icons-material/Thermostat';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
-import { Divider, Grid2, Paper, Skeleton, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from "@mui/material";
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import { Divider, Paper, Skeleton, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import useCustomTheme from "../../hooks/useCustomTheme";
 import Weather from "../../interfaces/Weather";
@@ -13,30 +13,6 @@ const capitals = [
     "Florianópolis", "Fortaleza", "João Pessoa", "Manaus", "Natal", "Porto Alegre", "Recife", "Rio de Janeiro", "Salvador",
     "São Luís", "São Paulo", "Teresina", "Vitória"
 ]
-
-interface Props {
-    disappearOnMobile?: boolean
-}
-
-const GridIcons = ({ disappearOnMobile }: Props): React.JSX.Element => {
-    return (
-        <Grid2
-            size={{ xs: 12, md: 6, sm: 6 }}
-            display={disappearOnMobile ? { xs: 'none', sm: 'flex' } : "flex"}
-            gap={7}
-        >
-            <Tooltip title="Temperature">
-                <ThermostatIcon />
-            </Tooltip>
-            <Tooltip title="Wind">
-                <AirIcon />
-            </Tooltip>
-            <Tooltip title="Climate">
-                <CloudIcon />
-            </Tooltip>
-        </Grid2>
-    )
-}
 
 const CapitalClimates = (): React.JSX.Element => {
     const { theme } = useCustomTheme();
@@ -68,87 +44,67 @@ const CapitalClimates = (): React.JSX.Element => {
         <Stack spacing={2} width={"100%"} >
             <Divider color={theme.palette.text.primary} variant='fullWidth' />
             <Typography variant='h4' textAlign={'left'} fontWeight={600}>Brazil Capitals</Typography>
-            {/* <Grid2 container>
-                <GridIcons />
-                <GridIcons disappearOnMobile />
-            </Grid2>
             {isLoading ? (
-                capitals.map((_value, index) => (
-                    <Skeleton key={index} variant='rounded' sx={{
-                        width: {
-                            xs: "100%"
-                        },
-                        height: "24px"
-                    }} />
-                ))
-            ) : (
-                <Grid2 container spacing={1}>
+                <Paper style={{ padding: '5px', display: 'flex', flexDirection: 'column', gap: '5px' }}>
                     {
-                        weatherData ? (
-                            capitals.map((capital, index) => (
-                                <Grid2 key={index} size={{ xs: 12, sm: 6 }}>
-                                    {weatherData[capital] && (
-                                        <Stack direction={'row'} spacing={2}>
-                                            <Typography>{weatherData[capital].temperature} ºC</Typography>
-                                            <Typography>{weatherData[capital].wind} km/h</Typography>
-                                            <Typography>{weatherData[capital].description}</Typography>
-                                            <Typography fontWeight={600}>{capital}</Typography>
-                                        </Stack>
-                                    )}
-                                </Grid2>
-                            ))
-                        ) : (
-                            <Typography variant="body2" color="error">Erro ao carregar dados</Typography>
-                        )
+                        capitals.map((_value, index) => (
+                            <Skeleton key={index} variant='rounded' sx={{
+                                width: {
+                                    xs: "100%"
+                                },
+                                height: "50px"
+                            }} />
+                        ))
                     }
-                </Grid2>
-            )} */}
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align='center'>
-                                <Tooltip title="Temperature">
-                                    <ThermostatIcon />
-                                </Tooltip>
-                            </TableCell>
-                            <TableCell align='center'>
-                                <Tooltip title="Wind">
-                                    <AirIcon />
-                                </Tooltip>
-                            </TableCell>
-                            <TableCell align='center'>
-                                <Tooltip title="Climate">
-                                    <CloudIcon />
-                                </Tooltip>
-                            </TableCell>
-                            <TableCell align='center'>
-                                <Tooltip title="City">
-                                    <LocationCityIcon />
-                                </Tooltip>
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            weatherData && (
-                                capitals.map((capital, index) => (
-                                    weatherData[capital] && (
-                                        <TableRow key={index}>
-                                            <TableCell align='center'>{weatherData[capital].temperature}</TableCell>
-                                            <TableCell align='center'>{weatherData[capital].wind}</TableCell>
-                                            <TableCell align='center'>{weatherData[capital].description}</TableCell>
-                                            <TableCell align='center'>
-                                                <Typography variant='body2' fontWeight={600}>{capital}</Typography>
-                                            </TableCell>
-                                        </TableRow>
-                                    )
-                                ))
-                            )
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                </Paper>
+            ) : (
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align='center'>
+                                    <Tooltip title="City">
+                                        <LocationCityIcon />
+                                    </Tooltip>
+                                </TableCell>
+                                <TableCell align='center'>
+                                    <Tooltip title="Temperature">
+                                        <ThermostatIcon />
+                                    </Tooltip>
+                                </TableCell>
+                                <TableCell align='center'>
+                                    <Tooltip title="Wind">
+                                        <AirIcon />
+                                    </Tooltip>
+                                </TableCell>
+                                <TableCell align='center'>
+                                    <Tooltip title="Climate">
+                                        <CloudIcon />
+                                    </Tooltip>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                weatherData && (
+                                    capitals.map((capital, index) => (
+                                        weatherData[capital] && (
+                                            <TableRow key={index}>
+                                                <TableCell align='center'>
+                                                    <Typography variant='body2' fontWeight={600}>{capital}</Typography>
+                                                </TableCell>
+                                                <TableCell align='center'>{weatherData[capital].temperature}</TableCell>
+                                                <TableCell align='center'>{weatherData[capital].wind}</TableCell>
+                                                <TableCell align='center'>{weatherData[capital].description}</TableCell>
+                                            </TableRow>
+                                        )
+                                    ))
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
         </Stack>
     );
 }
